e9c656d4c63b4d8e778a6d2f5b84be766023fec6
LucasSFeitosa <lucassousafeitosa@gmail.com>
2017-03-16 19:17:53 -0300

    V teste cheio de error

--- AMBA.sv
+++ AMBA.sv
@@ -3,42 +3,57 @@
 module AMBA(
 input clk, 
 
-logic [2:0] controle1,	//configura como leitura e escrita do BSG_CONTROL bits de 0 a 2
+output logic [2:0] controle1,	//configura como leitura e escrita do BSG_CONTROL bits de 0 a 2
 input logic [4:0] controle2,	//Configura o BSG_CONTROL dos bits 3 em diante com apenas leitura
-
-
 output logic [7:0] data1, data2,
-input logic [7:0] data_in, data_out, endereço,
-input logic valid, ready
+
+//comunicaç~ao AMBA
+input logic [7:0] data_in, endereco,
+output logic [7:0] data_out,
+input logic valid, 
+output ready
 );
 
-wire [7:0] controle;
-assign controle[2:0]=controle1;	//controle representa o BSG_CONTROL[7:0]
-assign controle[7:3]=controle2;
+logic [7:0] controle;		// 0 - TXENABLE,	1 - INTMSK, 2 - INTFLAG, 3 - STATUS
+ 
+always_comb
+	controle[2:0]=controle1;	//controle representa o BSG_CONTROL[7:0]
+	
+always_comb
+	controle[7:3]=controle2;
 
 always_comb
-	if (controle[3]==1'b0)
-		ready
+	if (controle[1] & controle[3])
+		controle[2]=1;	//INT_FLAG	BSG_CONTROL[2]
+	//else
+		//controle[2]=0;
+
+always_comb
+	if (controle[3]==1'b0)	//STATUS - 0 = n~ao est´a tendo transmiss~ao
+		ready=1'b1;
+	else
+		ready=1'b0;
 		
 
-always@(posedge clk)
+always_ff@(posedge clk)
 begin
       
-	if(valid)begin //se o dado for válido.
-		case(endereco)
-			8'h10: begin
-				ready = 1;
+	case(endereco)
+		8'h10: begin
+			data_out[7:0] = controle[7:0];
+			if(valid==1'b1 & ready==1'b1)	//Se tiver dado valido no barramento (valid=1) e n~ao estiver ocorrendo transmiss~ao (status=0) (ready=1)
 				controle[2:0] = data_in[2:0];
-			end
-			8'h11:begin
-				ready = 1;
+		end
+		8'h11:begin
+			data_out[7:0] = data1[7:0];
+			if(valid==1'b1 & ready==1'b1)
 				data1 = data_in;
-			end
-			8'h12:begin
-				ready = 1;
+		end
+		8'h12:begin
+			data_out[7:0] = data2[7:0];
+			if(valid==1'b1 & ready==1'b1)
 				data2 = data_in;
-			end
-		endcase
-	end
+		end
+	endcase
 end
-	
+endmodule
--- BSG.sv
+++ BSG.sv
@@ -2,8 +2,14 @@
 
 module BSG(	
 	input SYS_CLK, G_CLK_TX,
-	output logic [7:0]OUT	//Sinal modulado
-	output logic BSG_INT
+	output logic [7:0]OUT,	//Sinal modulado
+	output logic BSG_INT,
+	
+	//Abaixo -> comunicaç~ao AMBA
+	input logic [7:0]Data_in, addr,	//addr = endereço do registrador
+	output logic [7:0]Data_out,
+	input valid,
+	output ready
 );
 
 logic [7:0]BSG_CONTROL;	// 0 - TXENABLE,	1 - INTMSK, 2 - INTFLAG, 3 - STATUS
@@ -11,21 +17,19 @@ logic [7:0]BSG_DATA_0;
 logic [7:0]BSG_DATA_1;
 
 logic data_flag; // Especifica o BSG_DATA a ser lido;
-wire [7:0] BSG_DATA, DATA_GRAY;	//DATA_GRAY ´e apenas um fio para conectar a saida do encoder ao modulador
+logic [7:0] BSG_DATA, DATA_GRAY;	//DATA_GRAY ´e apenas um fio para conectar a saida do encoder ao modulador
 
 
-
-
-initial begin
+initial
 	data_flag = 0;
 
-end
 
-AMBA AMBA_H( .clk(SYS_CLK), .controle1(BSG_CONTROL[2:0]), .controle2(BSG_CONTROL[7:3]), .data1(BSG_DATA_0), .data2(BSG_DATA_1), .data_in(), .data_out(), .endereco(), .valid(), .ready());
+AMBA AMBA_H( .clk(SYS_CLK), .controle1(BSG_CONTROL[2:0]), .controle2(BSG_CONTROL[7:3]), .data1(BSG_DATA_0), .data2(BSG_DATA_1),
+ .data_in(Data_in), .data_out(Data_out), .endereco(addr), .valid(valid), .ready(ready));
 
 encoder encoder_h(.binario(BSG_DATA), .gray(DATA_GRAY));
 
-modulador modulador_h( .clk(G_CLK_TX), .saida(OUT), .DADO(DATA_GRAY), .flag_byte(data_flag), .status(BSG_CONTROL[3]);
+modulador modulador_h( .clk(G_CLK_TX), .saida(OUT), .DADO(DATA_GRAY), .flag_byte(data_flag), .status(BSG_CONTROL[3]));
 
 always_comb
 	case (data_flag)		//alterna qual resgistrador esta sendo enviado os dados para a modulaç~ao
@@ -33,7 +37,8 @@ always_comb
 		1'b1: BSG_DATA = BSG_DATA_1;
 	endcase
 
+always_comb
+	BSG_INT=BSG_CONTROL[2];		//Sinal de Interrupç~ao
 
-
-
+ 
 endmodule
